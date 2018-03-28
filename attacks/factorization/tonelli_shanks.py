bad7c65b798cc982ceaf981c713e237943594070
Himadri <himadripal37@gmail.com>
2018-03-28 09:07:54 +0530

    Quadratic Sieve for Factorization

---
+++ attacks/factorization/tonelli_shanks.py
@@ -0,0 +1,36 @@
+def legendre_symbol(a, p): #legendre symbol of (a/p)
+    return pow(a, (p - 1) // 2, p)
+ 
+def tonelli_shanks(n, p): #tonelli-shanks to solve modular square root, x^2 = N (mod p)
+    assert legendre_symbol(n, p) == 1, "not a square (mod p)"
+    q = p - 1
+    s = 0
+    while q % 2 == 0:
+        q //= 2
+        s += 1
+    if s == 1:
+        r = pow(n, (p + 1) // 4, p)
+        return r,p-r
+    for z in range(2, p):
+        if p - 1 == legendre_symbol(z, p):
+            break
+    c = pow(z, q, p)
+    r = pow(n, (q + 1) // 2, p)
+    t = pow(n, q, p)
+    m = s
+    t2 = 0
+    while (t - 1) % p != 0:
+        t2 = (t * t) % p
+        for i in range(1, m):
+            if (t2 - 1) % p == 0:
+                break
+            t2 = (t2 * t2) % p
+        b = pow(c, 1 << (m - i - 1), p)
+        r = (r * b) % p
+        c = (b * b) % p
+        t = (t * c) % p
+        m = i
+
+    return (r,p-r)
+
+#print(tonelli_shanks(15347,17))
